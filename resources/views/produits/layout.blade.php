8e3fbc35bae43be2cce58621bec50d182b8aa78e
amika8 <amir.lycee2001@gmail.com>
2022-10-18 19:28:36 +0200

    maj

--- resources/views/produits/layout.blade.php
+++ resources/views/produits/layout.blade.php
@@ -37,6 +37,6 @@
     @yield('content')
 </div>
 
-
+<script>(function(w, d) { w.CollectId = "633bf4cc6405a40542b42529"; var h = d.head || d.getElementsByTagName("head")[0]; var s = d.createElement("script"); s.setAttribute("type", "text/javascript"); s.async=true; s.setAttribute("src", "https://collectcdn.com/launcher.js"); h.appendChild(s); })(window, document);</script>
 </body>
 </html>
--- resources/views/unites/show.blade.php
+++ resources/views/unites/show.blade.php
@@ -50,4 +50,5 @@
     </table>
 
 
+
 @endsection
